5c8b352b30fe65cb309a1661bd3510d0f038eeae
AlbertoBujan <birtebs@gmail.com>
2025-08-07 08:44:18 +0200

    clean files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,11 @@ function Footer() {
       <img src="/assets/logo_pequeño.png" alt="logo" />
 
       <div className="linkedin">
-        <a href="https://www.linkedin.com/in/alberto-bujan-bb1bb0146/">
+        <a href="https://www.linkedin.com/in/alberto-bujan/">
           Alberto Buján<img src="/assets/linkedin.png" alt="linkedin"></img>
         </a>
-        <a href="https://www.linkedin.com/in/pablo-diaz-garcia-a25166200/">
-          Pablo Díaz<img src="/assets/linkedin.png" alt="linkedin"></img>
-        </a>
-        <a href="https://www.linkedin.com/in/jos%C3%A9-junquera-fdez-arg%C3%BCelles-b96280ba/">
-          José Junquera<img src="/assets/linkedin.png" alt="linkedin"></img>
+        <a href="https://www.linkedin.com/in/aitor-le%C3%B3n-a%C3%B1%C3%B3n-89748714b/">
+          Aitor León<img src="/assets/linkedin.png" alt="linkedin"></img>
         </a>
       </div>
     </footer>
